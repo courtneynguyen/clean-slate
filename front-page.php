4e1cbec1bbb2bcf0aec155dab876e98e8276e9a5
Courtney Nguyen <courtneythuy@gmail.com>
2016-11-27 21:21:29 -0700

    styling

--- front-page.php
+++ front-page.php
@@ -4,11 +4,45 @@
 </div>
 <div class="container">
 	<div class="row">
-		<div role="main" class="">
-			<h1>Writer. Engineer. Analyst.</h1>
+		<div role="main" class="content col-xs-12">
+			<h1>Cats are amazing</h1>
+			<h2>So are dogs</h2>
+			<h3>but cats are the best.</h3>
+			<h4>Ferrets are ok</h4>
+			<h5>Ducks go quack</h5>
+			<h6>Cows go moo!</h6>
+			<p>Mrow attack the dog then pretend like nothing happened. Lick sellotape kick up litter eat from dog's food. Sleep nap meow for food, then when human fills food dish, take a few bites of food and continue meowing curl up and sleep on the freshly laundered towels and thinking longingly about tuna brine massacre a bird in the living room and then look like the cutest and most innocent animal on the planet.
+			<p>Mrow attack the dog then pretend like nothing happened. Lick sellotape kick up litter eat from dog's food. Sleep nap meow for food, then when human fills food dish, take a few bites of food and continue meowing curl up and sleep on the freshly laundered towels and thinking longingly about tuna brine massacre a bird in the living room and then look like the cutest and most innocent animal on the planet.</p>
+			<div class="container container-small">
+			<p>Mrow attack the dog then pretend like nothing happened. Lick sellotape kick up litter eat from dog's food. Sleep nap meow for food, then when human fills food dish, take a few bites of food and continue meowing curl up and sleep on the freshly laundered towels and thinking longingly about tuna brine massacre a bird in the living room and then look like the cutest and most innocent animal on the planet.</p>
+		</div>
+			<form>
+
+				<label for="test">You are?:</label>
+				<input id="test" type="text" class="form input" value="Testing" />
+
+
+				<input id="yes" type="radio" name="hi" value="yes" />
+				<label for="yes">Yes</label>
+
+				<input id="no" type="radio" name="hi" value="no" />
+				<label for="no">No</label>
+
+				<input id="maybe" type="radio" name="hi" value="maybe" />
+				<label for="maybe">Maybe</label>
+
+				<input id="ok" type="radio" name="hi" value="ok!" />
+				<label for="ok">Ok!</label>
+
+				<input type="button" value="Click Me!@" />
+				<input type="submit" value="Submit" />
+				<button>Subscribe</button>
+				<input type="checkbox" />
+			</form>
+			<!-- <h1>Writer. Engineer. Analyst.</h1>
 			<p>Hi! I am Courtney and I am here to help make the web be a better place. Starting with content; Content I find on web pages are often long-winded and don't get the point across. Instead they are commands, not experiences. Ideally it should be: Anything worth reading is worth saying; But the web is littered with bad grammar, run-on sentences, biases, poor design, that cause the web to become a place difficult to discern information. Together, let's make a difference in fighting against all these follies, and value it more as a prized memoir, of us.</p>
-		<h2>A little about me</h2>
-			<p>I could make this website like other portfolios, and shift the tone to, "Why you should hire me", but I am not all about that life. I am here to prove a point. I am here to write, to document, and to display my expertise. Many things we may not agree with, but we're both human. We can be wrong, we can have a point, and we can have the capacity to understand each other.</p>
+			<h2>A little about me</h2>
+			<p>I could make this website like other portfolios, and shift the tone to, "Why you should hire me", but I am not all about that life. I am here to prove a point. I am here to write, to document, and to display my expertise. Many things we may not agree with, but we're both human. We can be wrong, we can have a point, and we can have the capacity to understand each other.</p> -->
 		</div>
 	</div>
 </div>
